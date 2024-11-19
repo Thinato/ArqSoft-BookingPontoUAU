a3551c6303453c28c699c11958bcfc4e15a0cd32
AndreLuisPLuz <andreluispluz@gmail.com>
2024-11-19 19:23:49 -0300

    feat: implemented paginated fetch on guest repository.

--- BookingService/Adapters/Data/Guest/GuestRepository.cs
+++ BookingService/Adapters/Data/Guest/GuestRepository.cs
@@ -1,17 +1,31 @@
-﻿using Domain.Guests.Entities;
+﻿using Data.Pagination;
+using Domain.Guests.Entities;
 using Domain.Guests.Ports;
 using Microsoft.EntityFrameworkCore;
+using Shared.Pagination;
 
 namespace Data.Guests
 {
     public class GuestRepository : IGuestRepository
     {
         private readonly HotelDbContext _hotelDbContext;
+        private readonly PaginationService _paginationService;
 
-        public GuestRepository(HotelDbContext hotelDbContext)
+        public GuestRepository(
+                HotelDbContext hotelDbContext,
+                PaginationService paginationService)
         {
             _hotelDbContext = hotelDbContext;
+            _paginationService = paginationService;
+        }
 
+        public async Task<(IEnumerable<Guest>, PaginationInfo)> GetPaginated(PaginationQuery pagination)
+        {
+            var result = await _paginationService.Paginate(
+                _hotelDbContext.Guests,
+                pagination.ToOptions());
+            
+            return result;
         }
 
         async Task<Guest> IGuestRepository.Create(Guest guest)
--- BookingService/Core/Domain/Guest/Ports/IGuestRepository.cs
+++ BookingService/Core/Domain/Guest/Ports/IGuestRepository.cs
@@ -1,9 +1,10 @@
 ﻿using Domain.Guests.Entities;
+using Shared.Pagination;
 
 namespace Domain.Guests.Ports;
 public interface IGuestRepository
 {
     Task<Guest?> Get(int Id);
     Task<Guest> Create(Guest guest);
-
+    Task<(IEnumerable<Guest>, PaginationInfo)> GetPaginated(PaginationQuery pagination);
 }
